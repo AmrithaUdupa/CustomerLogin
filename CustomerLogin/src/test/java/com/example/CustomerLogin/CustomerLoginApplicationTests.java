768537eba852006ffdfadb4edb76d086bb7dc4f1
AmrithaUdupa <udupaamritha54@gmail.com>
2024-06-24 18:34:49 +0530

    Created Project

---
+++ CustomerLogin/src/test/java/com/example/CustomerLogin/CustomerLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CustomerLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
